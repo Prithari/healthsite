a975811333714cc561e073151aa9f0d39c8b9e01
Prithari <158446456+Prithari@users.noreply.github.com>
2024-04-05 15:58:22 +0530

    Update columns.js

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -38,6 +38,22 @@ export default function decorate(block) {
           childchange[1].classList.add('col-md-9');
     }
 
+    var childDiv = document.querySelector('.row');
+    var childchange = childDiv.children;
+      // Append the new class name to the child element
+    if(childchange.length > 0){ 
+          childchange[0].classList.add('col-md-3');
+          childchange[1].classList.add('col-md-9');
+    }
+
+    var uiDiv = document.querySelector('.uirow');
+    var uichange = uiDiv.children;
+      // Append the new class name to the child element
+    if(uichange.length > 0){ 
+          uichange[0].classList.add('test1');
+          uichange[1].classList.add('test2');
+    }
+
  
   } else {
     console.error('Parent div with class "' + parentClass + '" not found.');
